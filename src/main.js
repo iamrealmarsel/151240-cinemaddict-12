c843cc00db11b84e1b0a6d06f141672668cdcb52
mars <maspam@yandex.ru>
2020-07-30 04:23:05 +0300

    отрисовал верстку на странице

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 'use strict'
 
+
 const renderDOM = (container, markup, place) => {
   container.insertAdjacentHTML(place, markup);
 }
@@ -99,5 +100,50 @@ const createFilmMostcommentedMarkup = () => {
 const createFooterStatMarkup = () => {
   return(`<p>130 291 movies inside</p>`);
 }
+
+
+const FILM_COUNT = 5;
+const FILM_EXTRA_COUNT = 2;
+
+
+const headerElement = document.querySelector('.header');
+const mainElement = document.querySelector('.main');
+const footerElement = document.querySelector('.footer');
+const footerStat = footerElement.querySelector('.footer__statistics')
+
+
+renderDOM(headerElement, createProfileMarkup(), 'beforeend');
+renderDOM(mainElement, createNavigationMarkup(), 'afterbegin');
+renderDOM(mainElement, createSortMarkup(), 'beforeend');
+renderDOM(mainElement, createFilmContainerMarkup(), 'beforeend');
+
+
+const filmContainer = mainElement.querySelector('.films');
+const filmList = filmContainer.querySelector('.films-list__container');
+
+
+for (let i = 0; i < FILM_COUNT; i++) {
+  renderDOM(filmList, createFilmCardMarkup(), 'beforeend');
+}
+
+renderDOM(filmList, createMoreButtonMarkup(), 'afterend');
+renderDOM(filmContainer, createFilmTopRatedMarkup(), 'beforeend');
+renderDOM(filmContainer, createFilmMostcommentedMarkup(), 'beforeend');
+
+
+const filmListExtra = filmContainer.querySelectorAll('.films-list--extra');
+const filmListTopRated = filmListExtra[0].querySelector('.films-list__container');
+const filmListMostcommented = filmListExtra[1].querySelector('.films-list__container');
+
+
+for (let i = 0; i < FILM_EXTRA_COUNT; i++) {
+  renderDOM(filmListTopRated, createFilmCardMarkup(), 'beforeend');
+}
+
+for (let i = 0; i < FILM_EXTRA_COUNT; i++) {
+  renderDOM(filmListMostcommented, createFilmCardMarkup(), 'beforeend');
+}
+
+renderDOM(footerStat, createFooterStatMarkup(), 'beforeend');
 
 
