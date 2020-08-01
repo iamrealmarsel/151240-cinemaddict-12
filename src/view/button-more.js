87e5287c425fed9d5ae0f6fdc0029c940b1f37f5
mars <maspam@yandex.ru>
2020-08-01 21:30:48 +0300

    разбил монолит на модули

---
+++ src/view/button-more.js
@@ -0,0 +1,6 @@
+const createButtonMoreMarkup = () => {
+  return(`<button class="films-list__show-more">Show more</button>`);
+}
+
+
+export {createButtonMoreMarkup}
---
+++ src/view/footer-stat.js
@@ -0,0 +1,6 @@
+const createFooterStatMarkup = () => {
+  return(`<p>130 291 movies inside</p>`);
+}
+
+
+export {createFooterStatMarkup}
